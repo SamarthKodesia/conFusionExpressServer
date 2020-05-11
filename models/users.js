558a5aa7011346427858ab0cd00f952a79fc7d12
Samarth Kodesia <samarthkodesia@gmail.com>
2020-05-12 01:07:58 +0530

    Secure Server https

--- models/users.js
+++ models/users.js
@@ -3,8 +3,16 @@ var Schema = mongoose.Schema;
 var passportLocalMongoose = require('passport-local-mongoose');
 
 var User = new Schema({
-    admin:{
-        type:Boolean,
+    firstname: {
+      type: String,
+        default: ''
+    },
+    lastname: {
+      type: String,
+        default: ''
+    },
+    admin:   {
+        type: Boolean,
         default: false
     }
 });
