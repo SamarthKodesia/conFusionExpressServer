5a42c5ecc695c877d137aadf038d504fadc130b9
Samarth Kodesia <samarthkodesia@gmail.com>
2020-05-14 02:11:29 +0530

    passport facebook

--- config.js
+++ config.js
@@ -1,4 +1,8 @@
 module.exports = {
     'secretKey': '12345-67890-09876-54321',
-    'mongoUrl' : 'mongodb://localhost:27017/conFusion'
+    'mongoUrl': 'mongodb://localhost:27017/conFusion',
+    'facebook': {
+        clientId: '4034850366586622',
+        clientSecret: 'ba99339a9abf6f803c7905b8f535b5b3'
+    }
 }
