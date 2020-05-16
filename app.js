51147ac69a60de340be0a5f2ca3df6b1a10152ac
Samarth Kodesia <samarthkodesia@gmail.com>
2020-05-17 00:52:37 +0530

    Favourites EndPoint

--- app.js
+++ app.js
@@ -15,6 +15,7 @@ var dishRouter = require('./routes/dishRouter');
 var promoRouter = require('./routes/promoRoute');
 var leaderRouter = require('./routes/leadersRoute');
 const uploadRouter = require('./routes/uploadRouter');
+const favoritesRouter = require('./routes/favoriteRouter');
 
 const mongoose = require('mongoose');
 
@@ -68,6 +69,7 @@ app.use('/dishes', dishRouter);
 app.use('/promotions', promoRouter);
 app.use('/leaders', leaderRouter);
 app.use('/imageUpload',uploadRouter);
+app.use('/favorites',favoritesRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
